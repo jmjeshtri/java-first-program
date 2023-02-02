a1350cf9d963007f1301991399a0b9ff54c6f59e
Jorida Mjeshtri <64932820+jmjeshtri@users.noreply.github.com>
2023-02-02 16:38:13 +0100

    Commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,7 +4,7 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -12,6 +12,15 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
+    }
+
+    private static int add(int[] numbers){
+//      method to find the sum of the numbers in an array
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum = sum + numbers[i];
+        }
+        return sum;
     }
 }
